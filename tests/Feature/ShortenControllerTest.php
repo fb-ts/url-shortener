5b513539798424c6f467d3cc7aa103d7637ce7fc
Tomasz Sidorczuk <tsidorczuk@9bits.pl>
2018-06-01 23:45:06 +0200

    test ShortenController: - shorten - validation fails - invalid url - validation fail when url is longer that max - validation success when url is equal max

--- tests/Feature/ShortenControllerTest.php
+++ tests/Feature/ShortenControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Hashids;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
 
 class ShortenControllerTest extends TestCase
@@ -11,7 +12,7 @@ class ShortenControllerTest extends TestCase
     use WithFaker;
 
     /**
-     * A basic test example.
+     * Shorten url
      *
      * @return void
      */
@@ -20,9 +21,91 @@ class ShortenControllerTest extends TestCase
         $url = $this->faker->url;
 
         $response = $this->post('/api/shorten', ['url' => $url]);
-        $response->assertExactJson(['success' => true]);
-
         $hash = $response->json('hash');
+
+        $response->assertStatus(200)
+            ->assertExactJson(['success' => true, 'hash' => $hash]);
+
         $this->assertDatabaseHas('shorteners', ['id' => Hashids::decode($hash), 'hash' => $hash, 'url' => $url]);
     }
+
+    /**
+     * Validation fails
+     *
+     * @return void
+     */
+    public function testValidationFails(): void
+    {
+        $response = $this->post('/api/shorten');
+
+        $response->assertSessionHasErrors(['url']);
+    }
+
+
+    /**
+     * Invalid url
+     *
+     * @return void
+     */
+    public function testInvalidUrl(): void
+    {
+        $this->withoutExceptionHandling();
+
+        $cases = ['//invalid-url.com', '/invalid-url', 'invalid-url.com'];
+
+        foreach ($cases as $case) {
+            try {
+                $this->post('/api/shorten', ['url' => $case]);
+            } catch (ValidationException $e) {
+                $this->assertEquals(
+                    'The url format is invalid.',
+                    $e->validator->errors()->first('url')
+                );
+                continue;
+            }
+
+            $this->fail("The URL $case passed validation when it should have failed.");
+        }
+    }
+
+    /**
+     * Validation fail when url is longer that max:20482
+     *
+     * @return void
+     */
+    public function testMaxLengthWhenTooLong(): void
+    {
+        $this->withoutExceptionHandling();
+
+        $url = 'http://';
+        $url .= str_repeat('a', 2049 - \strlen($url));
+
+        try {
+            $this->post('/api/shorten', ['url' => $url]);
+        } catch (ValidationException $e) {
+            $this->assertEquals(
+                'The url may not be greater than 2048 characters.',
+                $e->validator->errors()->first('url')
+            );
+            return;
+        }
+
+        $this->fail('Max length should trigger a ValidationException');
+    }
+
+    /**
+     * Validation success when url is longer is equal max:2048
+     *
+     * @return void
+     */
+    public function testMaxLengthWhenUnderMax(): void
+    {
+        $url = 'http://';
+        $url .= str_repeat('a', 2048 - \strlen($url));
+
+        $this->post('/api/shorten', ['url' => $url]);
+
+        $this->assertDatabaseHas('shorteners', ['url' => $url]);
+    }
+
 }
