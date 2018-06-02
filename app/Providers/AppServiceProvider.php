4ad14a638db4b0b534574ed1831934d41cf05aba
Tomasz Sidorczuk <tsidorczuk@9bits.pl>
2018-06-02 10:21:02 +0200

    observe model shortener

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace UrlShortener\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use UrlShortener\Observers\ShortenerObserver;
+use UrlShortener\Shortener;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Shortener::observe(ShortenerObserver::class);
     }
 
     /**
