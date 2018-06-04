87a865d5c1d2e85fcec09c37bf0acea5282a8910
Tomasz Sidorczuk <tsidorczuk@9bits.pl>
2018-06-04 09:15:38 +0200

    migration create table visits

---
+++ database/migrations/2018_06_04_064020_create_visits_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVisitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('shortener_id');
+            $table->date('date');
+            $table->unsignedInteger('count');
+            $table->timestamps();
+
+            $table->foreign('shortener_id')->references('id')->on('shorteners');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visits');
+    }
+}
